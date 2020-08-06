edce6f2eb5f41991e39e85ba7d44a5d0cb2a41a4
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-08-06 14:57:24 -0300

    Adds stack navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/landing';
 import { AppLoading } from 'expo';
+
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
-export default function App() {
-  let [fontsLoaded] = useFonts({
-    Archivo_400Regular,
-    Archivo_700Bold,
-    Poppins_400Regular,
-    Poppins_600SemiBold,
-  });
+import AppStack from './src/routes/app-stack';
 
-  return fontsLoaded
-    ? (
-      <>
-        <Landing />
-        <StatusBar style="light" />
-      </>
-    )
-    : <AppLoading />;
+export default function App() {
+    let [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_700Bold,
+        Poppins_400Regular,
+        Poppins_600SemiBold,
+    });
+
+    return fontsLoaded
+        ? (
+            <>
+                <AppStack />
+                <StatusBar style="light" />
+            </>
+        )
+        : <AppLoading />;
 }
